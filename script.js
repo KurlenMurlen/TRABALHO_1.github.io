2f08e593fb203d70b3f64474ef3d31bacef13807
KurlenMurlen <131370284+KurlenMurlen@users.noreply.github.com>
2023-06-14 18:29:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed("span", {
+    strings: ["Tarso Bertolini", "Aluno de ciência da computação"],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true,
+  });
+  
+  document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "https:youtube.com";
+  });
